1c211fdbd22f3f0120aaaff88880a45d6b1a8301
lucian.juratu00 <lucian.juratu00@e-uvt.ro>
2023-02-21 17:03:24 +0200

    added project files

---
+++ static/js/firebaseConfig.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+    databaseURL:""
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
